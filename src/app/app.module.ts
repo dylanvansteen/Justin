bb4aeae3b5e580fbe9b975d46bd7c61bd64b799d
Dylan van Steen <dylanvansteen@gmail.com>
2017-01-27 15:54:05 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
-import {HomeComponent } from './pages/home/home.component';
-import {DayOneComponent } from './pages/day-one/day-one.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DayOneComponent } from './pages/day-one/day-one.component';
 
-import {DayTwoComponent } from './pages/day-two/day-two.component';
-import {DayThreeComponent } from './pages/day-three/day-three.component';
-import {DayFourComponent } from './pages/day-four/day-four.component';
-import {DayFiveComponent } from './pages/day-five/day-five.component';
+import { DayTwoComponent } from './pages/day-two/day-two.component';
+import { DayThreeComponent } from './pages/day-three/day-three.component';
+import { DayFourComponent } from './pages/day-four/day-four.component';
+import { DayFiveComponent } from './pages/day-five/day-five.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,15 +31,17 @@ import {DayFiveComponent } from './pages/day-five/day-five.component';
     RouterModule.forRoot([
       { path: 'home', component: HomeComponent },
       { path: 'day-one', component: DayOneComponent },
-            { path: 'day-two', component: DayTwoComponent },
-            { path: 'day-three', component: DayThreeComponent },
-            { path: 'day-four', component: DayFourComponent },
-             { path: 'day-five', component: DayFiveComponent },
+      { path: 'day-two', component: DayTwoComponent },
+      { path: 'day-three', component: DayThreeComponent },
+      { path: 'day-four', component: DayFourComponent },
+      { path: 'day-five', component: DayFiveComponent },
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: '**', redirectTo: 'home', pathMatch: 'full' }
     ])
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
